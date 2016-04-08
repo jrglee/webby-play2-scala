8ef33a3e8452f349dfa2db9fc101b2d58c7481bd
Y. Wayne Huang <wayne530@gmail.com>
2016-04-08 09:24:33 -0700

    replace stub data with actual

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import java.net.{InetAddress, UnknownHostException}
 import javax.inject.Inject
 
 import play.api.libs.json.Json
@@ -8,8 +9,18 @@ import models.Location._
 
 class HomeController @Inject extends Controller {
 
-  def index = Action {
-    Ok(Json.toJson(Location("1.2.3.4", "foo.bar")))
+  def index = Action { request =>
+    val ip = request.remoteAddress
+    Ok(Json.toJson(Location(ip, lookupHostnameFromIp(ip))))
+  }
+
+  def lookupHostnameFromIp(ip: String): String = {
+    try {
+      val addr = InetAddress.getByName(ip)
+      addr.getHostName
+    } catch {
+      case e: UnknownHostException => ip
+    }
   }
 
 }
